bd6f5e0ddb66e24881f5ec4ad1fb740057006b01
NeilBarnedo <neil.barnedo@hotelplan.co.uk>
2025-07-12 16:02:34 +0800

    modified:   .env 	modified:   composer.lock 	modified:   vendor/autoload.php 	modified:   vendor/bin/sail 	modified:   vendor/carbonphp/carbon-doctrine-types/composer.json 	modified:   vendor/carbonphp/carbon-doctrine-types/src/Carbon/Doctrine/CarbonDoctrineType.php 	modified:   vendor/carbonphp/carbon-doctrine-types/src/Carbon/Doctrine/CarbonImmutableType.php 	modified:   vendor/carbonphp/carbon-doctrine-types/src/Carbon/Doctrine/CarbonType.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -14,12 +14,9 @@ if (PHP_VERSION_ID < 50600) {
             echo $err;
         }
     }
-    trigger_error(
-        $err,
-        E_USER_ERROR
-    );
+    throw new RuntimeException($err);
 }
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitfe33f98a750b8c5a51b30c78bd3fab21::getLoader();
+return ComposerAutoloaderInit570ad57f5614831130dd86a445a2fe37::getLoader();
